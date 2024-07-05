9a1405e851f46558e62e65f0bfabc25ec0af95c9
Akshay Nair <phenax5@gmail.com>
2024-07-05 21:06:55 +0530

    Init commit with flake shit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      in {
+       devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            terraform
+            terraform-ls
+          ];
+        };
+      });
+}
+
